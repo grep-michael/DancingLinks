ac262e23d0bffad9db19ed99d03a1d4a9e78c8c5
michaelknudsen <mmknudsen@bsu.edu>
2021-04-28 14:09:20 -0400

    begun actual work

--- HelperFunctions.py
+++ HelperFunctions.py
@@ -1,3 +1,19 @@
+class NodeObject():
+    def __init__(self,L=None,R=None,U=None,D=None,C=None):
+        self.L=L
+        self.R=R
+        self.D=D
+        self.U=U
+
+class ColumnObject( NodeObject ):
+    def __init__(self,L=None,R=None,U=None,D=None,C=None,N="defaultName",S=-1):
+        super().__init__(L,R,U,D,C)
+        self.N=N
+        self.S=S
+class testClass():
+    def __init__(self,*args,**kwargs):
+        print(args)
+        print(kwargs)
 def pprint(matrix):
     #print column count
     sys.stdout.write(f"  ")
@@ -35,3 +51,15 @@ def testFillFromLecture():
         [0,1,1,0,0,1,1],
         [0,1,0,0,0,0,1]
     ]
+
+def ConvertMatrixToList():
+    pass
+
+
+if __name__ == "__main__":
+    #Used for testing
+    x = NodeObject(U=1,D=2,L=3,R=4,C=5)
+    c = ColumnObject(D=2,L=3,R=4,C=5,N=6,S=7)
+    #print(x.U)
+    #print(c.U)
+    x = testClass(1,2,3,4)
